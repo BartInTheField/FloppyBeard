d0c0d3812bd3ca944b3d1e5f295c129ed38f88cd
Bart in t Veld <bart.int.007@gmail.com>
2019-06-24 22:32:51 +0200

    Added button prompt

---
+++ Assets/Scripts/ImpulseFadeTextUI.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class ImpulseFadeTextUI : MonoBehaviour
+{
+    [SerializeField] private float fadeSpeed = 0.5f;
+
+    private bool doFade = true;
+    private TextMeshProUGUI textMesh;
+
+    private void Awake()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+        textMesh.alpha = 0;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(StartFading());
+    }
+
+    private IEnumerator StartFading()
+    {
+        textMesh.DOFade(1, fadeSpeed);
+
+        yield return new WaitForSeconds(fadeSpeed);
+
+        textMesh.DOFade(0, fadeSpeed);
+
+        yield return new WaitForSeconds(fadeSpeed);
+
+        if (doFade)
+        {
+            StartCoroutine(StartFading());
+        }
+    }
+
+    public void StopFading()
+    {
+        doFade = false;
+    }
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -9,18 +9,24 @@ public class UIManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI scoreText = null;
     [SerializeField] private GameObject gameOverPanelObject = null;
 
+    [SerializeField] private ImpulseFadeTextUI fadingButtonPrompt = null;
+
     private GameManager gameManager;
+    private InputManager inputManager;
 
     private void Awake()
     {
         // GameManager is always in the scene
         gameManager = FindObjectOfType<GameManager>();
+        // InputManager is always in the scene
+        inputManager = FindObjectOfType<InputManager>();
     }
 
     private void Start()
     {
         gameManager.OnScoreChanged += SetScore;
         gameManager.OnGameOver += ShowGameOver;
+        inputManager.OnPlayerReady += StopButtonPrompt;
     }
 
     private void SetScore(int score)
@@ -33,5 +39,10 @@ public class UIManager : MonoBehaviour
         gameOverPanelObject.SetActive(true);
         GameOverPanel gameOverPanel = gameOverPanelObject.GetComponent<GameOverPanel>();
         gameOverPanel.SetScore(finalScore);
+    }
+
+    private void StopButtonPrompt()
+    {
+        fadingButtonPrompt.StopFading();
     }
 }
